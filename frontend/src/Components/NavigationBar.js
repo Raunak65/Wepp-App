e77860334a629610ce30e3d3618eafb19ac9c7e8
Saket Maniyar <saketmaniyar@gmail.com>
2021-10-18 19:33:20 +0530

    navbar changed

--- frontend/src/Components/NavigationBar.js
+++ frontend/src/Components/NavigationBar.js
@@ -1,120 +1,15 @@
-import * as React from "react";
-import { styled, alpha } from "@mui/material/styles";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
-import SearchIcon from "@mui/icons-material/Search";
-import SideBar from "./SideBar";
-import BottomBar from "./BottomBar/BottomBar";
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "40%",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "auto",
-    },
-  },
-}));
-
-export default function NavigationBar() {
-  return (
-    <>
-      <React.Fragment>
-        <Box sx={{ flexGrow: 1 }}>
-          <AppBar position="static">
-            <Toolbar>
-              <SideBar />
-              <Typography
-                variant="h6"
-                noWrap
-                component="div"
-                sx={{ display: { xs: "none", sm: "block" } }}
-              >
-                Wepp
-              </Typography>
-              <Box sx={{ flexGrow: 1 }} />
-              <Search>
-                <SearchIconWrapper>
-                  <SearchIcon />
-                </SearchIconWrapper>
-                <StyledInputBase
-                  placeholder="Search"
-                  inputProps={{ "aria-label": "search" }}
-                />
-              </Search>
-              <Box sx={{ flexGrow: 1 }} />
-              <Box sx={{ display: { xs: "none", md: "flex" } }}>
-                <ShoppingCartOutlinedIcon />
-                Cart
-              </Box>
-              <Box ml={5} />
-              <Box sx={{ display: { xs: "none", md: "flex" } }}>
-                <LogoutOutlinedIcon />
-                Logout
-              </Box>
-            </Toolbar>
-          </AppBar>
-        </Box>
-      </React.Fragment>
-
-      <React.Fragment>
-        <BottomBar />
-      </React.Fragment>
-    </>
-  );
-}
-
-
-
 // import * as React from "react";
 // import { styled, alpha } from "@mui/material/styles";
 // import AppBar from "@mui/material/AppBar";
 // import Box from "@mui/material/Box";
 // import Toolbar from "@mui/material/Toolbar";
-// import IconButton from "@mui/material/IconButton";
 // import Typography from "@mui/material/Typography";
 // import InputBase from "@mui/material/InputBase";
-// import Badge from "@mui/material/Badge";
-// import MenuItem from "@mui/material/MenuItem";
-// import Menu from "@mui/material/Menu";
-// import SideBar from "./SideBar";
 // import SearchIcon from "@mui/icons-material/Search";
-// import AccountCircle from "@mui/icons-material/AccountCircle";
-// import MailIcon from "@mui/icons-material/Mail";
-// import NotificationsIcon from "@mui/icons-material/Notifications";
-// import MoreIcon from "@mui/icons-material/MoreVert";
+// import SideBar from "./SideBar";
+// import BottomBar from "./BottomBar/BottomBar";
+// import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+// import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 
 // const Search = styled("div")(({ theme }) => ({
 //   position: "relative",
@@ -123,12 +18,11 @@ export default function NavigationBar() {
 //   "&:hover": {
 //     backgroundColor: alpha(theme.palette.common.white, 0.25),
 //   },
-//   marginRight: theme.spacing(2),
 //   marginLeft: 0,
 //   width: "100%",
 //   [theme.breakpoints.up("sm")]: {
-//     marginLeft: theme.spacing(4),
-//     width: "20%",
+//     marginLeft: theme.spacing(1),
+//     width: "40%",
 //   },
 // }));
 
@@ -146,7 +40,6 @@ export default function NavigationBar() {
 //   color: "inherit",
 //   "& .MuiInputBase-input": {
 //     padding: theme.spacing(1, 1, 1, 0),
-//     // vertical padding + font size from searchIcon
 //     transition: theme.transitions.create("width"),
 //     width: "100%",
 //     [theme.breakpoints.up("md")]: {
@@ -155,176 +48,140 @@ export default function NavigationBar() {
 //   },
 // }));
 
-// export default function PrimarySearchAppBar() {
-//   const [anchorEl, setAnchorEl] = React.useState(null);
-//   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-
-//   const isMenuOpen = Boolean(anchorEl);
-//   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-//   const handleProfileMenuOpen = (event) => {
-//     setAnchorEl(event.currentTarget);
-//   };
-
-//   const handleMobileMenuClose = () => {
-//     setMobileMoreAnchorEl(null);
-//   };
-
-//   const handleMenuClose = () => {
-//     setAnchorEl(null);
-//     handleMobileMenuClose();
-//   };
-
-//   const handleMobileMenuOpen = (event) => {
-//     setMobileMoreAnchorEl(event.currentTarget);
-//   };
-
-//   const menuId = "primary-search-account-menu";
-//   const renderMenu = (
-//     <Menu
-//       anchorEl={anchorEl}
-//       anchorOrigin={{
-//         vertical: "top",
-//         horizontal: "right",
-//       }}
-//       id={menuId}
-//       keepMounted
-//       transformOrigin={{
-//         vertical: "top",
-//         horizontal: "right",
-//       }}
-//       open={isMenuOpen}
-//       onClose={handleMenuClose}
-//     >
-//       <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-//       <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-//     </Menu>
-//   );
-
-//   const mobileMenuId = "primary-search-account-menu-mobile";
-//   const renderMobileMenu = (
-//     <Menu
-//       anchorEl={mobileMoreAnchorEl}
-//       anchorOrigin={{
-//         vertical: "top",
-//         horizontal: "right",
-//       }}
-//       id={mobileMenuId}
-//       keepMounted
-//       transformOrigin={{
-//         vertical: "top",
-//         horizontal: "right",
-//       }}
-//       open={isMobileMenuOpen}
-//       onClose={handleMobileMenuClose}
-//     >
-//       <MenuItem>
-//         <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-//           <Badge badgeContent={4} color="error">
-//             <MailIcon />
-//           </Badge>
-//         </IconButton>
-//         <p>Messages</p>
-//       </MenuItem>
-//       <MenuItem>
-//         <IconButton
-//           size="large"
-//           aria-label="show 17 new notifications"
-//           color="inherit"
-//         >
-//           <Badge badgeContent={17} color="error">
-//             <NotificationsIcon />
-//           </Badge>
-//         </IconButton>
-//         <p>Notifications</p>
-//       </MenuItem>
-//       <MenuItem onClick={handleProfileMenuOpen}>
-//         <IconButton
-//           size="large"
-//           aria-label="account of current user"
-//           aria-controls="primary-search-account-menu"
-//           aria-haspopup="true"
-//           color="inherit"
-//         >
-//           <AccountCircle />
-//         </IconButton>
-//         <p>Profile</p>
-//       </MenuItem>
-//     </Menu>
-//   );
-
+// export default function NavigationBar() {
 //   return (
-//     <Box sx={{ flexGrow: 1 }}>
-//       <AppBar position="static">
-//         <Toolbar>
-//           <SideBar />
-//           <Typography
-//             variant="h6"
-//             noWrap
-//             component="div"
-//             sx={{ display: { xs: "none", sm: "block" } }}
-//           >
-//             MUI
-//           </Typography>
-//           <Search>
-//             <SearchIconWrapper>
-//               <SearchIcon />
-//             </SearchIconWrapper>
-//             <StyledInputBase
-//               placeholder="Search…"
-//               inputProps={{ "aria-label": "search" }}
-//             />
-//           </Search>
-//           <Box sx={{ flexGrow: 1 }} />
-//           <Box sx={{ display: { xs: "none", md: "flex" } }}>
-//             <IconButton
-//               size="large"
-//               aria-label="show 4 new mails"
-//               color="inherit"
-//             >
-//               <Badge badgeContent={4} color="error">
-//                 <MailIcon />
-//               </Badge>
-//             </IconButton>
-//             <IconButton
-//               size="large"
-//               aria-label="show 17 new notifications"
-//               color="inherit"
-//             >
-//               <Badge badgeContent={17} color="error">
-//                 <NotificationsIcon />
-//               </Badge>
-//             </IconButton>
-//             <IconButton
-//               size="large"
-//               edge="end"
-//               aria-label="account of current user"
-//               aria-controls={menuId}
-//               aria-haspopup="true"
-//               onClick={handleProfileMenuOpen}
-//               color="inherit"
-//             >
-//               <AccountCircle />
-//             </IconButton>
-//           </Box>
-//           <Box sx={{ display: { xs: "flex", md: "none" } }}>
-//             <IconButton
-//               size="large"
-//               aria-label="show more"
-//               aria-controls={mobileMenuId}
-//               aria-haspopup="true"
-//               onClick={handleMobileMenuOpen}
-//               color="inherit"
-//             >
-//               <MoreIcon />
-//             </IconButton>
-//           </Box>
-//         </Toolbar>
-//       </AppBar>
-//       {renderMobileMenu}
-//       {renderMenu}
-//     </Box>
+//     <>
+//       <React.Fragment>
+//         <Box sx={{ flexGrow: 1 }}>
+//           <AppBar position="static">
+//             <Toolbar>
+//               <SideBar />
+//               <Typography
+//                 variant="h6"
+//                 noWrap
+//                 component="div"
+//                 sx={{ display: { xs: "none", sm: "block" } }}
+//               >
+//                 Wepp
+//               </Typography>
+//               <Box sx={{ flexGrow: 1 }} />
+//               <Search>
+//                 <SearchIconWrapper>
+//                   <SearchIcon />
+//                 </SearchIconWrapper>
+//                 <StyledInputBase
+//                   placeholder="Search"
+//                   inputProps={{ "aria-label": "search" }}
+//                 />
+//               </Search>
+//               <Box sx={{ flexGrow: 1 }} />
+//               <Box sx={{ display: { xs: "none", md: "flex" } }}>
+//                 <ShoppingCartOutlinedIcon />
+//                 Cart
+//               </Box>
+//               <Box ml={5} />
+//               <Box sx={{ display: { xs: "none", md: "flex" } }}>
+//                 <LogoutOutlinedIcon />
+//                 Logout
+//               </Box>
+//             </Toolbar>
+//           </AppBar>
+//         </Box>
+//       </React.Fragment>
+
+//       <React.Fragment>
+//         <BottomBar />
+//       </React.Fragment>
+//     </>
 //   );
 // }
 
+import React, { useState } from "react";
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBBtn,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+  MDBDropdownLink,
+  MDBCollapse,
+} from "mdb-react-ui-kit";
+import BottomBar from "./BottomBar/BottomBar";
 
+export default function App() {
+  const [showBasic, setShowBasic] = useState(false);
+
+  return (
+    <>
+      <React.Fragment>
+        <MDBNavbar  position="static" expand="lg" light bgColor="light">
+          <MDBContainer fluid>
+            <MDBNavbarBrand href="#">Wepp</MDBNavbarBrand>
+            <MDBNavbarToggler
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+              onClick={() => setShowBasic(!showBasic)}
+            >
+              <MDBIcon icon="bars" fas />
+            </MDBNavbarToggler>
+
+            <MDBCollapse navbar show={showBasic}>
+              <MDBNavbarNav className="mr-auto mb-2 mb-lg-0">
+                <MDBNavbarItem>
+                  <MDBNavbarLink active aria-current="page" href="#">
+                    Home
+                  </MDBNavbarLink>
+                </MDBNavbarItem>
+              </MDBNavbarNav>
+              <form className="d-flex input-group w-50">
+                <input
+                  type="search"
+                  className="form-control"
+                  placeholder="Type query"
+                  aria-label="Search"
+                />
+                <MDBBtn color="primary">Search</MDBBtn>
+              </form>
+            </MDBCollapse>
+          </MDBContainer>
+
+          <MDBCollapse navbar show={showBasic}>
+            <MDBNavbarNav right className="mr-auto mb-2 mb-lg-0">
+              <MDBNavbarItem>
+                <MDBDropdown>
+                  <MDBDropdownToggle tag="a" className="nav-link">
+                    <MDBIcon fas icon="user-circle" size="lg" />
+                  </MDBDropdownToggle>
+                  <MDBDropdownMenu>
+                    <MDBDropdownItem>
+                      <MDBDropdownLink>My Profile</MDBDropdownLink>
+                    </MDBDropdownItem>
+                    <MDBDropdownItem>
+                      <MDBDropdownLink>WishList</MDBDropdownLink>
+                    </MDBDropdownItem>
+                    <MDBDropdownItem>
+                      <MDBDropdownLink>Something else here</MDBDropdownLink>
+                    </MDBDropdownItem>
+                  </MDBDropdownMenu>
+                </MDBDropdown>
+              </MDBNavbarItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
+        </MDBNavbar>
+      </React.Fragment>
+      
+      <React.Fragment>
+        <BottomBar />
+      </React.Fragment>
+    </>
+  );
+}
